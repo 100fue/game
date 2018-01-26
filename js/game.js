7c8283ea6d2bde5e4731ee9e29f3f31cd990d355
100fue <malvacien@gmail.com>
2018-01-26 02:29:31 +0100

    almost done

--- js/game.js
+++ js/game.js
@@ -1,9 +1,6 @@
 function Game() {
 
     this.player1 = new Player(100, 50, "./img/julio-i.png", "playerOne right", 48, 70);
-    // this.insectLeft = new Insects(950,Math.ceil(Math.random()* 100) + 150,30+"px",30+"px","./img/mosca-i-prueba.png",(Math.ceil(Math.random()*3)*-1),"insect");
-    // this.insectRight = new Insects(0,Math.ceil(Math.random()* 100) + 150,30+"px",30+"px","./img/mosca-d-prueba.png", Math.ceil(Math.random()*2)+3, "insect");
-    // this.monster1 = new Insects(950,Math.ceil(Math.random()* 100) + 150,50+"px",50+"px","./img/bee-i-prueba.png", Math.ceil(Math.random()*2)+3, "monster");
     this.tongue1 = new Tongue(this.player1.x + 35, this.player1.y + 42, "tongueOne");
     this.cont1 = new Board(50, 50);
     this.insOneLeft = [
